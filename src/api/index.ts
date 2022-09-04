ec3d9747bbb05370f4fb2092f94ae00396aa4ecf
Daniel Ariz <danielariz@Matiass-MBP.lan>
2022-09-04 01:36:59 -0300

    fix: extract QueryType to type file

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
+import { QueryType } from "../types";
 
 const newsUrl = "https://hn.algolia.com/api/v1/search_by_date?";
-
-type QueryType = "angular" | "reactjs" | "vuejs";
 
 export const getNews = (query: QueryType, page: number = 0) => {
     return axios.get(`${newsUrl}query=${query}&page=${page}`);
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,9 +2,21 @@ export type HeaderProps = {
     title: string;
 }
 
+export type ToggleProps = {
+    options: string[];
+}
+
+export type ToggleButtonProps = {
+    selected: boolean;
+}
+
 export type NewProps = {
+    story_id: number;
     created_at: string;
     author: string;
     story_title: string;
     story_url: string;
+    favourite: boolean;
 }
+
+export type QueryType = "" | "angular" | "reactjs" | "vuejs";
