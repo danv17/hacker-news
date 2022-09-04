911da0c44b8175658db3b39be4574bf1763de05b
Daniel Ariz <danielariz@Matiass-MBP.lan>
2022-09-04 01:38:05 -0300

    css: set background and border

--- src/style/Header.tsx
+++ src/style/Header.tsx
@@ -5,6 +5,8 @@ export const StyledHeaderContainer = styled.div`
     align-items: center;
     justify-content: flex-start;
     padding-left: 9.375rem;
+    box-shadow: 0 1px 4px 0 rgba(0, 21, 41, 0.12);
+    background-image: linear-gradient(to bottom, #ececec -32%, #fff 124%);
 `
 
 export const StyledHeader = styled.h1`
