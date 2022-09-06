548cf9830e82283c301df6c9b3df7688365cc659
Daniel Ariz <daniel.ariz.s@gmail.com>
2022-09-06 02:47:58 -0400

    css: adjust padding and margin

--- src/style/New.tsx
+++ src/style/New.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { ReactComponent as TimeSvg } from "../assets/time.svg";
 import { ReactComponent as HeartSvg } from "../assets/heart.svg";
 import { ReactComponent as HeartFullSvg } from "../assets/heart-full.svg";
-import { newWidth, newWidthSmall, linkLeftPadding, linkLeftPaddingSmall } from "./CssVariables";
+import { newWidth, newWidthSmall, linkLeftPadding, linkLeftPaddingSmall, linkPadding, linkPaddingSmall } from "./CssVariables";
 
 export const StyledNewContainer = styled.div`
   width: ${newWidth}%;
@@ -20,7 +20,7 @@ export const StyledNewContainer = styled.div`
     transition: opacity 0.15s ease-in-out;
   }
 
-  @media (max-width: 768px) {
+  @media (max-width: 900px) {
     width: ${newWidthSmall}%;
   }
 `;
@@ -33,12 +33,12 @@ export const StyledLinkContainer = styled.a`
   min-height: 4.625rem;
   background-color: #fff;
   text-decoration: none;
-  padding-left: ${linkLeftPadding}rem;
+  padding: ${linkPadding}rem ${linkPadding}rem ${linkPadding}rem ${linkLeftPadding}rem;
   border-top-left-radius: 6px;
   border-bottom-left-radius: 6px;
 
-  @media (max-width: 768px) {
-    padding-left: ${linkLeftPaddingSmall}rem;
+  @media (max-width: 900px) {
+    padding: ${linkPaddingSmall}rem ${linkPaddingSmall}rem ${linkPaddingSmall}rem ${linkLeftPaddingSmall}rem;
   }
 `;
 
@@ -46,7 +46,7 @@ export const StyledTimeAgoByAuthor = styled.div`
   display: flex;
   justify-content: flex-start;
   align-items: center;
-  font-size: 0.875rem;
+  font-size: 0.688rem;
   font-weight: 500;
   font-stretch: normal;
   font-style: normal;
@@ -65,6 +65,7 @@ export const StyledStoryTitle = styled.div`
   line-height: 1.43;
   letter-spacing: 0.25px;
   color: #6b6b6b;
+  margin-top: 0.375rem;
 `;
 
 export const TimeIcon = styled(TimeSvg)`
